dd753166a4acc062b3c4750a21271778cc96c121
mothibimalebana <mothibi.mm12@gmail.com>
2024-05-03 23:55:29 +0200

    starting from scratch, with new ideas :)

--- README.md
+++
@@ -1,2 +0,0 @@
-# Etch-a-sketch
-A browser version of something between a sketchpad and an Etch-A-Sketch.
--- script.js
+++
@@ -1,8 +0,0 @@
-//Container
-const container = document.querySelector("container");
-//Grids
-function createGrids(noOfGrids) {
-	for (let i = 0; i < noOfGrids; i++) {
-		container.appendChild(document.createElement("div"));
-	}
-}
